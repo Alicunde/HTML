7577ebd46c7fc89ad9e54ee6cb0a207262aca018
salbatore <46431183+Lucia-Marcos@users.noreply.github.com>
2020-12-14 00:44:45 +0100

    Filters

--- navigator/src/application/Constructor/logo.js
+++ navigator/src/application/Constructor/logo.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 
 class Module extends Component {
+    constructor(props){
+        super(props);
+        this.state={filter:'normal'}
+    }
     counter(version) {
         let items = this.props.items;
         if(!items)return 0;
@@ -20,12 +24,31 @@ class Module extends Component {
             return 0;
         }
     }
+    setFilter(f){
+        let b = document.body;
+        if(b.filter)document.body.classList.remove(b.filter);
+        let c = 'filter-'+f;
+        this.setState({filter:f})
+        b.filter = c;
+        b.classList.add(c);
+    }
     render() {
         return (
             <React.Fragment>
+                
                 <s>Components selected <b>0</b></s>
-                <p>by <b>Alicunde Systems</b> in 2020 in Madrid, Spain.<br/><br/>License: none (public domain)</p>
+                <div class="filters">
+                <div className={(this.state.filter==='invert')?'active':''} onClick={()=>{this.setFilter('invert');}}>BLACK</div>
+                <div className={(this.state.filter==='hacker')?'active':''} onClick={()=>{this.setFilter('hacker');}}>HACKER</div>
+                <div className={(this.state.filter==='antiq')?'active':''} onClick={()=>{this.setFilter('antiq');}}>ANTIQ</div>
+                <div className={(this.state.filter==='horror')?'active':''} onClick={()=>{this.setFilter('horror');}}>HORROR</div>
+                <div className={(this.state.filter==='destroy')?'active':''} onClick={()=>{this.setFilter('destroy');}}>DESTROY</div>
+                <div className={(this.state.filter==='normal')?'active':''} onClick={()=>{this.setFilter('normal');}}>NORMAL</div>
+                </div>
+                <p>Do you want to add an amazing component? ... create an issue in the github repository.</p>
+                <p>by <b>Salbatore Munuera</b> in 2020 in Madrid, Spain.<br/><br/>License: none (public domain)</p>
                 <p style={{color:'#000'}}>We translate the design into HTML in a simplified way. This is a beta system built in June 2020.</p>
+               
             </React.Fragment>
         );
     }
